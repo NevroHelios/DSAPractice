7b181f59b12073dc55f02436e65c915ee224425d
Arka Dash <138326606+NevroHelios@users.noreply.github.com>
2023-11-18 01:10:33 +0530

    Updates

--- str.cpp
+++ str.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0, n = s.size();
-
-        while(s[i] == ' '){ // skipping space characters at the beginning
+        int i = 0; int l = s.size();
+        while(s[i] == ' '){
             i++;
         }
 
-        int positive = 0, negative = 0; 
-
-        if(s[i] == '+'){
-            positive++; // number of positive signs at the start in string
+        int pos = 0; int neg = 0;
+        if (s[i] == '-'){
+            neg += 1;
+            i++;
+        }
+        if (s[i] == '+'){
+            pos += 1;
             i++;
         }
 
-        if(s[i] == '-'){
-            negative++; // number of negative signs at the start in string
+        double num = 0;
+        while(i < l && s[i] >= '0' && s[i] <= '9'){
+            num = num*10 + (s[i] - '0');
             i++;
         }
 
-        double ans = 0; 
-
-        while(i<n and s[i] >= '0' and s[i] <= '9'){
-            ans = ans*10 + (s[i] - '0'); // (s[i] - '0') is converting character to integer
-            i++;
-        }
-
-        if(negative > 0) // if negative sign exist
-            ans = -ans;
-        if(positive > 0 and negative > 0) // if both +ve and -ve sign exist, Example: +-12
+        if (neg > 0 and pos > 0){
             return 0;
+        }
+        if(neg > 0){
+            num = -num;
+        }
 
-        if(ans > INT_MAX) // if ans > 2^31 - 1
-            ans = INT_MAX;
 
-        if(ans < INT_MIN) // if ans < 2^31
-            ans = INT_MIN;
-
-        return (int)ans;
+        if (num > INT_MAX){
+            return INT_MAX;
+        }
+        if (num < INT_MIN){
+            return INT_MIN;
+        }
+        
+        return (int)num;
     }
 };

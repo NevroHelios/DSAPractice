2efb965319f9a2b658e07c59fd2e0fb65a24b33f
NevroHelios <dasharka05@gmail.com>
2023-12-04 19:16:09 +0530

    pattern

--- Ninja_And_The_Number_Pattern.cpp
+++ Ninja_And_The_Number_Pattern.cpp
@@ -6,21 +6,21 @@ int main()
 {
     int n = 4;
     
-    for (int i = 0; i < (int)(2*n-1)/2; i++){
-        for (int j = 0; j < (int)(2*n-1)/2; j++){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-1; j++){
             cout<<std::max(n-i, n-j);
         }
-        for (int j = (int)(2*n-1)/2; j > -1; j--){
+        for (int j = n-1; j > -1; j--){
             cout<<std::max(n-i, n-j);
         }
     cout<<endl;
     }
     
-    for (int i = (int)(2*n-1)/2; i > -1; i--){
+    for (int i = n-1; i > -1; i--){
         for (int j = 0; j < (int)(2*n-1)/2; j++){
             cout<<std::max(n-i, n-j);
         }
-        for (int j = (int)(2*n-1)/2; j > -1; j--){
+        for (int j = n-1; j > -1; j--){
             cout<<std::max(n-i, n-j);
         }
     cout<<endl;

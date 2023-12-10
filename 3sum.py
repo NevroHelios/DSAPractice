161cf4d6685c4d4bfbe591926277223a0abb6d5d
NevroHelios <dasharka05@gmail.com>
2023-12-11 01:06:12 +0530

    3sum

---
+++ 3sum.py
@@ -0,0 +1,30 @@
+def three_sum(nums):
+    nums.sort()
+    result = []
+    
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        j = i + 1
+        k = len(nums) - 1
+
+        while j < k:
+            total = nums[i] + nums[j] + nums[k]
+            if total > 0:
+                k -= 1
+            elif total < 0:
+                j += 1
+            else:
+                result.append([nums[i], nums[j], nums[k]])
+                j += 1
+                k -= 1
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+                while j < k and nums[k] == nums[k + 1]:
+                    k -= 1
+
+    return result
+
+nums = [-2, 0, 1, 1, 2]
+print(three_sum(nums))
